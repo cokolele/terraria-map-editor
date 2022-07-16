3810de091523b43d0b919d67e5e1fc8079f5356b
BinaryConstruct <BinaryConstruct@gmail.com>
2022-07-15 22:39:19 -0500

    fork
    Remove user accounts
    Remove server api requirements
    Change links to github issues

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -20,15 +20,16 @@ function Menu({ stateChange, stateToggle, view, running, user, unsafe, unsafeOnl
             type: "link",
             onClick: menu.onNewFile,
          },
-         "Open example map": {
-            type: "menu",
-            items: {
-               "Normal world": () => { menu.onExampleMap("normal") },
-               "Drunk world (seed)": () => { menu.onExampleMap("drunk") },
-               "Bees world (seed)": () => { menu.onExampleMap("bees") },
-               "For the worthy world (seed)": () => { menu.onExampleMap("good") }
-            }
-         },
+         // TODO: Disabled for fork
+         // "Open example map": {
+         //    type: "menu",
+         //    items: {
+         //       "Normal world": () => { menu.onExampleMap("normal") },
+         //       "Drunk world (seed)": () => { menu.onExampleMap("drunk") },
+         //       "Bees world (seed)": () => { menu.onExampleMap("bees") },
+         //       "For the worthy world (seed)": () => { menu.onExampleMap("good") }
+         //    }
+         // },
          "Save map image": {
             type: "button",
             enabled: running,
@@ -151,11 +152,11 @@ function Menu({ stateChange, stateToggle, view, running, user, unsafe, unsafeOnl
       Report: {
          "Error": {
             type: "link",
-            onClick: () => { stateChange("modal", "errorreport") }
+            onClick: () => { window.open("https://github.com/TEdit/terraria-map-editor-web/issues", '_blank') }
          },
          "Suggestions or feature requests": {
             type: "link",
-            onClick: () => { stateChange("modal", "suggestionreport") }
+            onClick: () => { window.open("https://github.com/TEdit/terraria-map-editor-web/issues", '_blank') }
          }
       }
    };
@@ -229,12 +230,13 @@ function Menu({ stateChange, stateToggle, view, running, user, unsafe, unsafeOnl
                label="Changelog"
                onClick={() => stateChange("modal", "changelog")}
             />
+            {/*
             <InputLink
                className="menu-link"
                label={user !== null ? user.username : "Account"}
                Icon={<AccountBoxIcon size={15}/>}
                onClick={() => stateChange("modal", user ? "account" : "signin")}
-            />
+            />*/}
             <InputLink
                className="menu-link"
                label="Discord"
